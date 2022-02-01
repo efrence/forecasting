b055c4510bfa1610e12490d9e4b420161173ae88
Efren Carbajal <efren_ce@hotmail.com>
2022-02-01 14:58:22 -0300

    Add redis-object gem as dependancy

--- Gemfile
+++ Gemfile
@@ -30,8 +30,9 @@ gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 gem "redis", "~> 4.0"
 
-# Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
-# gem "kredis"
+gem 'redis-objects', "~> 1.5"
+
+gem 'connection_pool', "~>2.2"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
---
+++ config/initializers/redis.rb
@@ -0,0 +1,9 @@
+require 'connection_pool'
+
+REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys
+default = REDIS_CONFIG[:default].symbolize_keys
+config = default.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
+
+Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(:host => config[:host], :port => config[:port]) }
+
+Redis::Objects.redis(&:flushdb) if Rails.env == "test"
